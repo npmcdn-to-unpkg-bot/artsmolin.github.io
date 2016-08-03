06728e599b3b4aa809491c47ad57c8b7c5c67c58
artursmolin <a.reg@outlook.com>
2016-08-03 18:35:20 +0300

    name == null

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -1,11 +1,11 @@
 var HelloWorld = React.createClass({
     render: function() {
-        if (name_ok == 1){
+        if (name != null){
             return (
                 <p><h1>Hello {name}</h1></p>
             );
         }
-        else {
+        if (name == null) {
             return (
                 <p><a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a></p>
             ); 
