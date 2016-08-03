f50a579b5e2947ab42aa68a9e611354fa7c9fa5c
artursmolin <a.reg@outlook.com>
2016-08-03 18:30:35 +0300

    name_ok

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -1,12 +1,16 @@
 var HelloWorld = React.createClass({
     render: function() {
-        return (
-            <p>
-              <h1>Hello {name}</h1>
-              <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
-            </p>
-          );
+        if (name_ok == 1){
+            return (
+                <p><h1>Hello {name}</h1></p>
+            );
         }
+        else {
+            return (
+                <p><a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a></p>
+            ); 
+        }
+    }
     });
 
 ReactDOM.render(
