8342bd1763a6795bf2f0429d8677420a2f5b5189
Artur Smolin <a.reg@outlook.com>
2016-08-03 21:51:58 +0400

    Update AuthApp.jsx

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -1,19 +1,16 @@
 var TestApp = React.createClass({
     render: function() {
-        if(getCookie("name") == undefined || getCookie("lastname") == undefined){
-            return (
-                <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
-            );
-        }
-        if(getCookie("name") != undefined && getCookie("lastname") != undefined){
-            return (
-                <h1>{getCookie("name") + " " + getCookie("lastname")} </h1>
-            );
-        }
         if (name == undefined || lastname == undefined){
-            return (
-                <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
-            );
+            if(getCookie("name") == undefined || getCookie("lastname") == undefined){
+                return (
+                    <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
+                );
+            }
+            if(getCookie("name") != undefined && getCookie("lastname") != undefined){
+                return (
+                    <h1>{getCookie("name") + " " + getCookie("lastname")} </h1>
+                );
+            }
         } 
         if (name != undefined && lastname != undefined){
             return (
