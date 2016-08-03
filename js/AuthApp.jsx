778391c5cb8dc161fb13d78a71a5850a3d8813d3
Artur Smolin <a.reg@outlook.com>
2016-08-03 20:42:06 +0400

    Update AuthApp.jsx

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -1,6 +1,5 @@
 var TestApp = React.createClass({
     render: function() {
-        init();
         if (name == undefined || lastname == undefined){
             return (
                 <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
