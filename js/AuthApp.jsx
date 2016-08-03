79d4af93608633a35c0d48a76fb0c1fe3c66e9c7
artursmolin <a.reg@outlook.com>
2016-08-03 17:58:55 +0300

    17:58

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -2,6 +2,7 @@ var HelloWorld = React.createClass({
     render: function() {
         return (
             <p>
+              <a href="https://oauth.vk.com/authorize?client_id=5572694&display=page&redirect_uri=http://artsmolin.github.io&scope=friends&response_type=token&v=5.53">Войти через ВК</a>
               Hello, <input type="text" placeholder="Your nameы here" />!
               It is {this.props.date.toTimeString() + name}
             </p>
