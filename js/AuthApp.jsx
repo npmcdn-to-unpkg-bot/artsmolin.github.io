e129977e4dd76e0691f6ead8dc6337e2c690d3cf
artursmolin <a.reg@outlook.com>
2016-08-03 17:56:11 +0300

    New Structure

--- js/AuthApp.jsx
+++ js/AuthApp.jsx
@@ -1,15 +1,15 @@
 var HelloWorld = React.createClass({
-        render: function() {
-          return (
+    render: function() {
+        return (
             <p>
               Hello, <input type="text" placeholder="Your nameы here" />!
-              It is {this.props.date.toTimeString()}
+              It is {this.props.date.toTimeString() + name}
             </p>
           );
         }
-      });
+    });
 
-        ReactDOM.render(
-          <HelloWorld date={new Date()} />,
-          document.getElementById('example')
-        );
+ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('example')
+);
---
+++ js/GetData.js
@@ -0,0 +1,17 @@
+function getData(){
+    var url = window.location.hash;
+    var arr = url.split("=");
+    var pre_token = arr[1];
+    var arr_token = pre_token.split("&");
+    var token = arr_token[0];
+    var id = arr[3];
+    var name = ""
+
+    var script = document.createElement('SCRIPT'); 
+    script.src = "https://api.vk.com/method/users.get?user_ids=" + id + "&fields=bdate&v=5.53&callback=callbackFunc"; 
+    document.getElementsByTagName("head")[0].appendChild(script); 
+}
+
+function callbackFunc(result) {
+  name = result.response[0].first_name;
+}
